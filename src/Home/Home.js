070bf1726a5a950fc1e848ec28cc229969408b60
Basoc Nicoleta <53331094+basocnicoletanina@users.noreply.github.com>
2019-08-21 12:41:19 +0300

    Implimented Header (#36)
    * header
    
    * Add Package-lock gitignore
    
    * made changes after review
    
    * modificated card-style to card

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "../Card/Card.js";
 
 function Home() {
-  return <Link to="about">Go To About Page</Link>;
+  return (
+    <div>
+      <Link to="about">Go To About Page</Link>
+      <Card sar="bhsdfvhv"></Card>
+    </div>
+  );
 }
-
 export default Home;
