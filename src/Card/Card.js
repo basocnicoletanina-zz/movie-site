1f7cab2dbf7f4e11dfeea7466b21541ce1bc97b6
Nicoleta Basoc <nbasoc@Ellations-iMac-5.local>
2019-08-29 14:38:28 +0300

    made scroll to top

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,43 +2,52 @@ import React from "react";
 import "./Card.css";
 import { Link } from "react-router-dom";
 
-function Card(props) {
-  return (
-    <div className="card">
-      {props.src ? (
-        <img src={props.src} className="image" />
-      ) : (
-        <img
-          className="no-image"
-          src="https://s3-ap-southeast-1.amazonaws.com/silverscreen-photos/1534489151m000001.jpg"
-        />
-      )}
-      <div className="text-card">
-        <p className="title">{props.title}</p>
+class Card extends React.Component {
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
 
-        {props.genres.length > 0 ? (
-          <ul className="genre">
-            {props.genres.map((genre, index) => (
-              <li key={index} className="genre-item">
-                {genre}
-              </li>
-            ))}
-          </ul>
+  render(props) {
+    return (
+      <div className="card">
+        {this.props.src ? (
+          <img src={this.props.src} className="image" />
         ) : (
-          <p className="no-genres-card">No genres available.</p>
+          <img
+            className="no-image"
+            src="https://s3-ap-southeast-1.amazonaws.com/silverscreen-photos/1534489151m000001.jpg"
+          />
         )}
+        <div className="text-card">
+          <p className="title">{this.props.title}</p>
 
-        {props.description ? (
-          <p className="text">{props.description}</p>
-        ) : (
-          <p className="plot-unknown-card">Plot Unknown.</p>
-        )}
+          {this.props.genres.length > 0 ? (
+            <ul className="genre">
+              {this.props.genres.map((genre, index) => (
+                <li key={index} className="genre-item">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="no-genres-card">No genres available.</p>
+          )}
 
-        <Link to={props.to} className="button">
-          Read more
-        </Link>
+          {this.props.description ? (
+            <p className="text">{this.props.description}</p>
+          ) : (
+            <p className="plot-unknown-card">Plot Unknown.</p>
+          )}
+
+          <Link
+            onClick={this.scrollToTop.bind(this)}
+            to={this.props.to}
+            className="button">
+            Read more
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default Card;
